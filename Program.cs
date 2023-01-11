c61bae5ee5f33aa7ca52344f231cd9be48691082
gharjare <gharjarechetan@gmail.com>
2023-01-11 13:22:40 +0530

    [chetan gharjare]: write wlcm msg

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace IndianCensusAnalyser
+{
+    class program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Indian Census Analyser");
+        }
+    }
+}
