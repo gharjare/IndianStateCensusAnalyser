b5908591d2307bff878f4d5ca370f43b3023666d
gharjare <gharjarechetan@gmail.com>
2023-01-11 13:28:19 +0530

    [chetan gharjare]:Here i store Csv adapter factory data

---
+++ CSVAdapterFactory.cs
@@ -0,0 +1,24 @@
+﻿using IndianCensusAnalyser.DTO;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IndianCensusAnalyser
+{
+    public class CSVAdapterFactory
+    {
+        public Dictionary<string, CensusDTO> LoadCsvData(CensusAnalyser.Country country, string csvFilePath, string dataHeader)
+        {
+            switch (country)
+            {
+                case (CensusAnalyser.Country.INDIA):
+                    return new IndianCensusAdapter().LoadCensusData(csvFilePath, dataHeader);
+
+                default:
+                    throw new CensusAnalyserException("No Such Country", CensusAnalyserException.ExceptionType.NO_SUCH_COUNTRY);
+            }
+        }
+    }
+}
