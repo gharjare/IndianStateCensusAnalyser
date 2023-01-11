53eddc40a5d112fb7beb9e3e35041cd38d30d766
gharjare <gharjarechetan@gmail.com>
2023-01-11 13:27:14 +0530

    [chetan gharjare]:Here i store census adapter data

---
+++ CensusAdapter.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace IndianCensusAnalyser
+{
+    public class CensusAdapter
+    {
+        protected string[] GetCensusData(string csvFilePath, string dataHeader)
+        {
+            string[] censusData;
+            if (!File.Exists(csvFilePath))
+            {
+                throw new CensusAnalyserException("File not found", CensusAnalyserException.ExceptionType.FILE_NOT_FOUND);
+            }
+            if (Path.GetExtension(csvFilePath) != ".csv")
+            {
+                throw new CensusAnalyserException("Invalid File Type", CensusAnalyserException.ExceptionType.INVALID_FILE_TYPE);
+            }
+            censusData = File.ReadAllLines(csvFilePath);
+            if (censusData[0] != dataHeader)
+            {
+                throw new CensusAnalyserException("Incorrect header in data", CensusAnalyserException.ExceptionType.INCORRECT_HEADER);
+            }
+            return censusData;
+        }
+    }
+}
