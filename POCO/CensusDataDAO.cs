3e5ce22a0ea10701f127fd0e2588382db8be40f1
gharjare <gharjarechetan@gmail.com>
2023-01-11 13:31:41 +0530

    [chetan gharjare]:Here we store census data DAo

---
+++ POCO/CensusDataDAO.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IndianCensusAnalyser.POCO
+{
+    public class CensusDataDAO
+    {
+        public string state;
+        public long population;
+        public long area;
+        public long density;
+
+        public CensusDataDAO(string state, long population, long area, long density)
+        {
+            this.state = state;
+            this.population = Convert.ToInt32(population);
+            this.area = Convert.ToInt32(area);
+
+            this.density = Convert.ToInt32(density);
+        }
+    }
+}
