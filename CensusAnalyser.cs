036dedc168b3713f55881d783437d61439fd9d2c
gharjare <gharjarechetan@gmail.com>
2023-01-11 13:26:03 +0530

    [chetan gharjare]:Here i write census analyser data

---
+++ CensusAnalyser.cs
@@ -0,0 +1,25 @@
+﻿using IndianCensusAnalyser.DTO;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IndianCensusAnalyser
+{
+    public class CensusAnalyser
+    {
+        public enum Country
+        {
+            INDIA, US, BRAZIL
+        }
+
+        Dictionary<string, CensusDTO> dataMap;
+
+        public Dictionary<string, CensusDTO> LoadCensusData(Country country, string csvFilePath, string dataHeader)
+        {
+            dataMap = new CSVAdapterFactory().LoadCsvData(country, csvFilePath, dataHeader);
+            return dataMap;
+        }
+    }
+}
